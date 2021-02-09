443cf9d78a00bb11327fa3a6c48da37750f80d11
Matthew Beaumont <mbeaumont3@dxc.com>
2021-02-09 12:28:30 +0000

    created the React JS template in a folder called my-app using "npx create-react-app my-app"

--- Javascript/javascript1.js
+++ Javascript/javascript1.js
@@ -1,2 +1,6 @@
 function function2() {
     document.getElementById("external").innerHTML="Paragraph has been changed by the external Javascript";}
+
+function function3(){
+  window.alert("meltdown Imminent")
+}    
