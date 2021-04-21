29404f3d4b9cb5ac9a008c44cf3d9c2f9392023c
Matthew <mbeaumont3@dxc.com>
2021-04-21 11:00:55 +0100

    npm installs

--- my first react/src/components/Children/Child1.component/Child1.component.jsx
+++ my first react/src/components/Children/Child1.component/Child1.component.jsx
@@ -4,9 +4,9 @@ export default function Child1() {
   return (
     <>
       <p style={{ backgroundColor: "red", height: "50px" }}>Child1 sentence</p>
-      <div>{property1}</div>
+      {/* <div>{property1}</div> */}
       {/* children is a key word property. If children is used then it allows a component to have nested components inside of it */}
-      {children}
+      {/* {children} */}
     </>
   );
 }
